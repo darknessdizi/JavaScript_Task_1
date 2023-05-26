70f8680eeffa6a68dd782c3c7406b5edfcaac64d
Remezov Dmitriy <darknessdizi@gmail.com>
2023-05-26 21:56:56 +0700

    Finished project

---
+++ index.js
@@ -0,0 +1,19 @@
+// document.write('Hello, world!')
+// console.log('Hello, developers!')
+
+const numberForUser = Math.floor(Math.random() * 1000)
+console.log("Исходное число: ", numberForUser)
+while (true) {
+    let numberFromUser = +prompt("Введите загаданное число: ")
+    console.log("Число пользователя: ", numberFromUser)
+    if (isNaN(numberFromUser)) {
+        alert("Вы ввели не число")
+    } else if (numberFromUser === numberForUser) {
+        alert("Вы угадали !!!")
+        break
+    } else if (numberFromUser < numberForUser) {
+        alert("Ваше число меньше.")
+    } else if (numberFromUser > numberForUser) {
+        alert("Ваше число больше.")
+    }
+}
